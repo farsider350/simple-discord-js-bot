782d6d03997e0ca5333694854fabe5f4c34f2096
farsider350 <31876349+farsider350@users.noreply.github.com>
2020-11-15 07:44:13 +1000

    conf

---
+++ commands/faucet.js
@@ -0,0 +1,19 @@
+/**
+ * This class responds to anyone that types !faucet aus and chooses one of the phrases below to respond with at random.
+ *
+ */
+module.exports = {
+    name: 'aus', // The name of the command
+    description: 'Australia Cash Faucet', // The description of the command (for help text)
+    args: false, // Specified that this command doesn't need any data other than the command
+    usage: '', // Help text to explain how to use the command (if it had any arguments)
+    execute(message, args) {
+
+        // List of phrases to respond with
+        var phrases = [
+            '!tipaus ' + message.author.id + ' 0.25'
+        ];
+
+        message.reply(phrases[Math.floor(Math.random()*phrases.length)]); // Replies to the user with a random phrase
+    },
+};
